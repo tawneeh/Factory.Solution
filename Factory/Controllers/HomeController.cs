d24793b15666f3f92042947447095411accda9dc
Tawnee Harris <tawneeh@icloud.com>
2021-01-11 18:34:43 -0800

    add ViewBag lists of both models

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller 
   {
+
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Engineer = _db.Engineers.ToList();
+      ViewBag.Machine = _db.Machines.ToList();
       return View(); 
     }
   }
